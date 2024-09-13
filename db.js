90f4f7185b7d8757cce8d9d8310fd14b05be3880
SolarisQuests <160103412+SolarisQuests@users.noreply.github.com>
2024-09-13 19:37:09 +0530

    done

---
+++ db.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose");
+const dotenv=require("dotenv")
+
+dotenv.config();
+
+
+
+const Connection = () => {
+  const MONGO_URI = process.env.MONGO_URI;
+
+  mongoose.connect(MONGO_URI, { useNewUrlParser: true });
+
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected successfully");
+  });
+  mongoose.connection.on("Disconnected", () => {
+    console.log("Database disconnected");
+  });
+  mongoose.connection.on("error", () => {
+    console.log("Error while connecting database");
+  });
+};
+
+
+module.exports = Connection;
